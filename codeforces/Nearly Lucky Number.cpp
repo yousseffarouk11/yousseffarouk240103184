d935cbb42be3940f3528bcff20cfe7d653075c45
yousseffarouk11 <yousseffarouk2468@gmail.com>
2025-12-17 00:36:23 +0200

    update codeforces

---
+++ codeforces/Nearly Lucky Number.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string n;
+    cin >> n;
+
+    int luckyCount = 0;
+
+    // عدّ الأرقام 4 و 7 فقط
+    for (char c : n) {
+        if (c == '4' || c == '7') luckyCount++;
+    }
+    string countStr = to_string(luckyCount);
+    bool isLucky = true;
+    for (char c : countStr) {
+        if (c != '4' && c != '7') {
+            isLucky = false;
+            break;
+        }
+    }
+
+    if (isLucky)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
